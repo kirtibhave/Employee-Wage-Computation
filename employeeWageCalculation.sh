065761ab4804a7edad32bd61a3fc389daaa2cfc7
Kirtibhave@123 <user.kirtibhave1000@gmail.com>
2020-03-04 17:35:20 +0530

    ADDED:Function to calculate daily employee wage

--- employeeWageCalculation.sh
+++ employeeWageCalculation.sh
@@ -1,2 +1,18 @@
-echo "Welcome to Employee Wage Computation Program"
+#!/bin/bash
+echo "**********Welcome to Employee Wage Computation Program**********"
 
+#CONSTANTS
+readonly EMPLOYEE_IS_PRESENT=1
+
+#Function is used to check employee is present or not
+function checkPresentOrAbsent(){
+randomCheck=$((RANDOM%2));
+	if [ $EMPLOYEE_IS_PRESENT -eq $randomCheck ]
+	then
+		echo "employee is present"
+	else
+		echo "employee is absent"
+	fi
+}
+
+checkPresentOrAbsent
