793b342b441a9c98730e9a1adda5e7128409516f
Kirtibhave@123 <user.kirtibhave1000@gmail.com>
2020-03-04 17:55:40 +0530

    MODIFIED:Function to check daily employee wage

--- employeeWageCalculation.sh
+++ employeeWageCalculation.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "**********Welcome to Employee Wage Computation Program**********"
 
 #CONSTANTS
 readonly EMPLOYEE_IS_PRESENT=1
 
-#Function is used to check employee is present or not
-function checkPresentOrAbsent(){
-randomCheck=$((RANDOM%2));
-	if [ $EMPLOYEE_IS_PRESENT -eq $randomCheck ]
-	then
-		echo "employee is present"
-	else
-		echo "employee is absent"
-	fi
-}
+#VARIABLES
+empHrs=8
+empWagePerHr=20
 
-checkPresentOrAbsent
+#Function is used to check employee is present or not
+function calculateDailyEmployeeWage(){
+randomCheck=$((RANDOM%2));
+if [ $EMPLOYEE_IS_PRESENT -eq $randomCheck ]
+then
+	salary=$(($empHrs * $empWagePerHr))
+else
+	salary=0
+fi
+	echo "Daily employee wage is: $salary"
+}
+calculateDailyEmployeeWage
